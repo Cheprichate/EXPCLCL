8b8946174f4744fe71cd0ab306c1dbc5b6c4ee1c
Cheprichate <petrovich15071998@mail.ru>
2023-09-28 20:59:55 +0400

    Добавьте файлы проекта.

---
+++ EXPCLCL.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+int e;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Сколько опыта вы получили ?";
+    std::cin >> e;
+    if (e < 1000) {
+        std::cout << "У вас 1 уровень.";
+    }
+    else if (e <= 2500){
+        std::cout << "У вас 2 уровень.";
+    }
+    else if (e <= 5000){
+        std::cout << "У вас 3 уровень.";
+    }
+    if (e > 5000){
+        std::cout << "У вас 4 уровень.";
+    }
+}
